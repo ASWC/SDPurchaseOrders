b041575a75aa7a754178c546bc2996ad9e183701
jmas2323 <jmas2323@ext148mac.local.superduperinc.com>
2019-12-02 11:58:16 -0500

    di set

---
+++ Model/Ui/ConfigProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace SDMagentoModules\SDPurchaseOrder\Model\Ui;
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\SamplePaymentGateway\Gateway\Http\Client\ClientMock;
+
+final class ConfigProvider implements ConfigProviderInterface
+{
+    const CODE = 'sample_gateway';
+    /**
+     * Retrieve assoc array of checkout configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return [
+            'payment' => [
+                self::CODE => [
+                    'transactionResults' => [
+                        ClientMock::SUCCESS => __('Success'),
+                        ClientMock::FAILURE => __('Fraud')
+                    ]
+                ]
+            ]
+        ];
+    }
+}
